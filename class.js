b621872f155ecd0d6476ffc8de6cfa2be5ea25b4
seolran <leesugar90@gmail.com>
2019-05-14 16:49:16 +0900

    [JavaScript 객체 지향 프로그래밍] + super

--- class.js
+++ class.js
@@ -1,36 +1,44 @@
-// 객체를 만드는 공장 class
 class Person{
-    constructor(name, first, second){  // 약속된 이름. 이 함수가 객체가 생성되는 과정에서 자동적으로 실행된다.
-        this.name=name;
-        this.first=first;
-        this.second=second;
+
+    // 객체가 만들어지기 직전에 실행되도록 약속되어 있는 함수.
+    // 약속된 이름이다.
+    constructor(name, first, second){
+        this.name = name;
+        this.first = first;
+        this.second = second;
     }
+
+    // 메소드를 만들 때에는 function 이라는 키워드를 쓰지 않는다.
+    // 2. 해당 객체에 sum이 없을때 여기를 바라본다.
     sum(){
         return this.first+this.second;
     }
-    // avg(){
-    //     return (this.first+this.second)/2
-    // }
-    
 }
 
-class PersonPlus extends Person{
+// class는 객체가 만들어지는 공장이다.
 
-    // super
-    // 부모가 갖고 있는 기능을 실행하면서 나만 하는 기능을 실행할 수 있도록
 
-    constructor(name, first, second, third){  // 약속된 이름. 이 함수가 객체가 생성되는 과정에서 자동적으로 실행된다.
-        super(name, first, second); // 부모 class의 constructor(생성자)를 실행+자식 기능 추가
-        this.third=third;
+class PersonPlus extends Person{    // Person과 중복되는 코드를 지울 수 있다.
+    
+    // 부모가 갖고 있는 기능을 실행한다.
+    // 나만 갖고 있는 기능을 추가한다.
+
+    constructor(name, first, second, third){
+        super(name, first, second); // 부모 클래스 생성자 안에서 호출된다.
+        this.third = third;   // only 자식이 해줄 일
     }
+
     sum(){
-        return super.sum()+this.third   // 부모 class의 sum을 실행+자식 기능 추가
+        return super.sum()+this.third;
+        // 부모가 갖고 있는 sum이 호출되고 그 리턴값에 자식의 추가적인 작업을 한다.
     }
+    
+    
     avg(){
-        return (this.first+this.second+this.third)/3
+        return (this.first+this.second+this.third)/3;
     }
 }
 
 var kim = new PersonPlus('kim', 10, 20, 30);
-console.log('kim.sum()', kim.sum())
-console.log('kim.avg()', kim.avg())
+console.log('kim.sum()', kim.sum());
+console.log('kim.avg()', kim.avg());
--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -1,10 +1,12 @@
 var superObj = {superVal:'super'}
-var subObj = {subVal:'sub'}
-subObj.__proto__ = superObj;    // prototype, __proto__
-console.log('subObj.subVal =>', subObj.subVal);
-console.log('subObj.superVal =>', subObj.superVal);
-console.log('subObj =>', subObj);
+// var subObj = {subVal:'sub'}
+// subObj.__proto__ = superObj;    // __proto__링크
+var subObj = Object.create(superObj);
+// console.log('subObj.subVal =>', subObj.subVal);
+// console.log('subObj.superVal =>', subObj.superVal);
+// console.log('subObj =>', subObj);
 
 subObj.superVal = 'sub';
+debugger;
 console.log('subObj.superVal =>', subObj.superVal); // sub
 console.log('superObj.superVal =>', superObj.superVal); // super(바뀌지 않는다)
