710e1e484b7d3cf45033b228377bca4189fe485c
seolran <leesugar90@gmail.com>
2019-05-14 17:56:49 +0900

    [JavaScript 객체 지향 프로그래밍] + 객체 간의 상속

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -1,12 +1,48 @@
-var superObj = {superVal:'super'}
-// var subObj = {subVal:'sub'}
-// subObj.__proto__ = superObj;    // __proto__링크
-var subObj = Object.create(superObj);
-// console.log('subObj.subVal =>', subObj.subVal);
-// console.log('subObj.superVal =>', subObj.superVal);
-// console.log('subObj =>', subObj);
+var superObj = {superVal: 'super'};
+// var subObj = {subVal: 'sub'};
+// subObj.__proto__ = superObj;
 
-subObj.superVal = 'sub';
+// 표준의 방법
+// superObj를 부모로 하는 새로운 객체 subObj
+var subObj = Object.create(superObj);
+subObj.subVal = 'sub';
 debugger;
-console.log('subObj.superVal =>', subObj.superVal); // sub
-console.log('superObj.superVal =>', superObj.superVal); // super(바뀌지 않는다)
+console.log('subObj.subVal: ',subObj.subVal);
+console.log('subObj.superVal: ',subObj.superVal);
+
+// 객체를 바꾸는 거지 그 객체의 프로토를 바꾸는 게 아니다.
+subObj.superVal = 'sub';
+
+console.log('subObj.superVal: ',subObj.superVal);
+console.log('superObj.superVal: ',superObj.superVal);
+
+var kim ={
+    name: 'kim',
+    first: 10, second: 20,
+    sum: function () {
+        return this.first+this.second
+    }
+};
+
+// lee.__proto__ = kim;
+
+var lee = Object.create(kim);
+lee.name = 'lee';
+lee.first = 10;
+lee.second = 10;
+lee.avg = function(){
+    return (this.first + this.second) / 2
+};
+// var lee = {
+//     name: 'lee',
+//     first: 10, second: 10,
+//     avg: function () {
+//         return (this.first + this.second)/2
+//     }
+// };
+
+
+
+console.log('lee.sum(): ',lee.sum());
+console.log('lee.avg(): ',lee.avg());
+// lee.sum():  20
