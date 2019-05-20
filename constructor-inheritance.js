078b1004161d09306595a274275d178f69c83155
seolran <leesugar90@gmail.com>
2019-05-20 11:12:15 +0900

    [JavaScript 객체 지향 프로그래밍] + 생성자를 통한 상속   - 생성자 함수를 통한 상속 : 부모 생성자 실행

---
+++ constructor-inheritance.js
@@ -0,0 +1,22 @@
+function Person(name, first, second){
+    this.name = name;
+    this.first = first;
+    this.second = second;
+}
+
+Person.prototype.sum = function () {
+    return this.first + this.second;
+};
+
+function PersonPlus(name, first, second, third) {
+    Person.call(this, name, first, second); // this(context, 문맥)을 지정해준다.
+    this.third = third;
+}
+
+PersonPlus.prototype.avg = function () {
+    return (this.first + this.second + this.third) / 3;
+};
+
+var kim = new PersonPlus('kim', 10, 20, 30);
+// console.log('kim.sum()', kim.sum());
+console.log('kim.avg()', kim.avg());
