0ae96b5ce16230f381aa238391bc3527ef58088c
seolran <leesugar90@gmail.com>
2019-05-20 11:35:22 +0900

    [JavaScript 객체 지향 프로그래밍] + 생성자를 통한 상속   - 생성자 함수를 통한 상속 : 부모 생성자 실행   - 생성자 함수를 통한 상속 : constructor 속성 바로잡기

--- constructor-inheritance.js
+++ constructor-inheritance.js
@@ -13,10 +13,26 @@ function PersonPlus(name, first, second, third) {
     this.third = third;
 }
 
+// PersonPlus.prototype.__proto__ = Person.prototype;
+PersonPlus.prototype = Object.create(Person.prototype); // 이게 표준
+
+PersonPlus.prototype.constructor = PersonPlus;
+// PersonPlus.prototype = Object.create(Person.prototype)를 쓰고 이걸 해주지 않으면 kim의 생성자 함수가 Person을 가리키게 된다. 왜냐면 PersonPlus.prototype이걸 아예 replace 하기 때문이다.
+
+// PersonPlus.prototype.sum = function () {
+//     return PersonPlus.prototype.__proto__.sum()+this.third;
+// };
+
 PersonPlus.prototype.avg = function () {
     return (this.first + this.second + this.third) / 3;
 };
 
+
 var kim = new PersonPlus('kim', 10, 20, 30);
-// console.log('kim.sum()', kim.sum());
+console.log('kim.sum()', kim.sum());
 console.log('kim.avg()', kim.avg());
+console.log('kim.constructor', kim.constructor);
+
+
+var lee = new Person('lee', 10, 20);
+console.log('lee.sum()', lee.sum());
